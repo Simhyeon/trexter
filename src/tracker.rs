49b24530a00f7e0e75fd399237d4b24327e92c2a
simoncreek <simoncreek@tutanota.com>
2022-07-11 18:33:41 +0900

    Now track requires generic T which impls clone and copy

--- src/tracker.rs
+++ src/tracker.rs
@@ -1,15 +1,20 @@
-#[derive(Default)]
-pub struct Tracker {
-    distance: Track,
-    tracks: Vec<Track>,
+pub struct Tracker<T> {
+    distance: Track<T>,
+    tracks: Vec<Track<T>>,
 }
 
-impl Tracker {
-    pub fn new() -> Self {
-        Self::default()
+impl<T> Tracker<T>
+where
+    T: Clone + Copy,
+{
+    pub fn new(start: T) -> Self {
+        Self {
+            distance: Track::new(start),
+            tracks: vec![],
+        }
     }
 
-    pub fn get_full_track(&self) -> Track {
+    pub fn get_full_track(&self) -> Track<T> {
         let mut full_track = self.distance;
 
         for track in &self.tracks {
@@ -19,17 +24,21 @@ impl Tracker {
         full_track
     }
 
-    pub fn get_track(&self) -> Track {
-        *self.get_last_track()
+    pub fn get_track(&self) -> &Track<T> {
+        if self.tracks.is_empty() {
+            &self.distance
+        } else {
+            self.tracks.last().unwrap()
+        }
     }
 
-    /// Merge last two tracks
+    /// connect last two tracks
     ///
     /// # Return
     ///
     /// - True if suceeded to merge
     /// - false if not
-    pub fn merge_track(&mut self) -> bool {
+    pub fn connect_track(&mut self) -> bool {
         if self.tracks.is_empty() {
             return false;
         }
@@ -58,7 +67,7 @@ impl Tracker {
         track.char_index += 1;
     }
 
-    fn get_last_track_mut(&mut self) -> &mut Track {
+    fn get_last_track_mut(&mut self) -> &mut Track<T> {
         if self.tracks.is_empty() {
             &mut self.distance
         } else {
@@ -66,32 +75,29 @@ impl Tracker {
         }
     }
 
-    fn get_last_track(&self) -> &Track {
-        if self.tracks.is_empty() {
-            &self.distance
-        } else {
-            self.tracks.last().unwrap()
+    pub fn set_milestone(&mut self, milestone: T) {
+        let track = Track::new(milestone);
+        self.tracks.push(track);
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Track<T> {
+    pub line_index: usize,
+    pub char_index: usize,
+    pub milestone: T,
+}
+
+impl<T> Track<T> {
+    pub fn new(milestone: T) -> Self {
+        Self {
+            line_index: 0,
+            char_index: 0,
+            milestone,
         }
     }
 
-    pub fn set_milestone(&mut self) {
-        let offset = Track::new();
-        self.tracks.push(offset);
-    }
-}
-
-#[derive(Debug, Default, Clone, Copy)]
-pub struct Track {
-    pub line_index: usize,
-    pub char_index: usize,
-}
-
-impl Track {
-    pub fn new() -> Self {
-        Self::default()
-    }
-
-    pub fn merge(&mut self, track: Track) {
+    pub fn merge(&mut self, track: Track<T>) {
         self.line_index += track.line_index;
         self.char_index += track.char_index;
     }
